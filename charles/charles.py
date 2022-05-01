bc11a940e27c6b4292a5ab552b468b7182c1d3ca
helenado <68400909+helenado@users.noreply.github.com>
2022-05-01 13:59:59 +0100

    .

---
+++ charles/charles.py
@@ -0,0 +1,80 @@
+from random import choice, random
+import numpy as np
+from copy import deepcopy
+
+
+class Individual:
+    def __init__(
+        self,
+        representation=None,
+        initial_sudoku=None,
+        valid_set=[i for i in range(1,10)]
+                ):
+        self.fitness = self.get_fitness()
+
+        if initial_sudoku is None:
+            raise Exception(
+                "It is mandatory to provide an initial sudoku."
+            )
+
+        if np.sqrt(len(initial_sudoku)).is_integer(): # check if it ia a square
+            raise Exception(
+                "The Sudoku provided must be quadratic"
+            )
+
+        self.index_missing = [i for i in range(len(initial_sudoku)) if initial_sudoku[i]==0]
+
+        if representation == None:# If a representation is not assigned, a possible solution is generated
+            new_sudoku = initial_sudoku.deepcopy()
+            # For each row
+            for j in np.arange(0, 81, 9):
+                number_missing = valid_set.deepcopy()
+                for i in range(j, j+9):  # indexes for each 'row'
+                    if i not in self.index_missing:# removing the numbers given from a list of possible numbers to fill
+                        number_missing.remove(initial_sudoku[i])
+                for i in range(j, j+9):
+                    if i in self.index_missing:
+                        new_sudoku[i] = choice(number_missing) #assigning a random number for each missing value (0)
+                        number_missing.remove(new_sudoku[i])
+            self.representation = new_sudoku.copy()
+
+        else:# if representation is defined by the user
+            self.representation = representation
+
+        self.fitness = self.get_fitness()
+
+    def get_fitness(self):
+        raise Exception("You need to monkey patch the fitness path.")
+
+    def get_neighbours(self, func, **kwargs):
+        raise Exception("You need to monkey patch the neighbourhood function.")
+
+    def __len__(self):
+        return len(self.representation)
+
+    def __getitem__(self, position):
+        return self.representation[position]
+
+    def __setitem__(self, position, value):
+        self.representation[position] = value
+
+    def __repr__(self):
+        return f"Individual(size={len(self.representation)}); Fitness: {self.fitness}"
+
+class Population:
+    def __init__(self, initial_sudoku, size, optim, **kwargs):
+        self.individuals = []
+        self.size = size
+        self.optim = optim
+        self.initial_sudoku = initial_sudoku
+        self.gen = 1
+
+        for _ in range(size):
+            self.individuals.append(
+                Individual(
+                    grid_initial=initial_sudoku,
+                    size=kwargs["sol_size"],
+                    valid_set=kwargs["valid_set"],
+                )
+            )
+
---
+++ main.py
@@ -0,0 +1,51 @@
+import numpy as np
+from copy import deepcopy
+
+def get_fitness(self):
+    """ This function computes fitness scores of each individual (a possible solution).
+    Since it consideres the number of unique digits in each row, column and box, the maximum fitness is 243 (81*3)
+
+    Returns:
+        fitness_value (int): the fitness for a sudoku solution
+    """
+
+    #initaializing and declaring a variable that will store the fitness value of a solution
+    fitness_value = 0
+
+    # get rows' fitness
+    for row in np.arange(0, 81, 9):
+        fitness_value += len(set(self.representation[row:row+9]))
+
+    # get columns' fitness
+    for columns in range(0, 9):
+        column_evaluate = []
+        for elem in [index + columns for index in np.arange(0, 81, 9)]:
+            column_evaluate.append(self.representation[elem])
+        fitness_value += len(set(column_evaluate))
+
+    # get boxs' fitness
+    # creating these variables to make the code susceptible to different Sudoku sizes
+    base = np.sqrt(np.sqrt(len(self.representaion)))  # 3
+    len_box = base ** 2  # 9
+    len_line_box = len_box * base  # 27
+
+    for k in range(0, base): # first 3 blocks, second  blocks, third three blocks
+            box_partial = deepcopy(self.representaion)
+            box_partial = box_partial[k*len_line_box:k*len_line_box+len_line_box]
+            print(box_partial)
+            for i in range(0, base):# for each block
+                    box = []
+                    for j in range(0, base): #for each line of the block
+                            box.append(box_partial[j*len_box + i*base:j*len_box + i*base+base])
+                    box = [x for sublist in box for x in sublist]
+                    fitness_value += len(set(box))
+
+    return fitness_value
+
+# the neighborhood does not apply for this specific problem
+def get_neighbours(self):
+    pass
+
+# monkey patching
+Individual.get_fitness = get_fitness
+Individual.get_neighbours = get_neighbours
