a9afc7c36b66cb385860491643b88027c59ee9f8
Filipa <79151739+filipacarreira@users.noreply.github.com>
2022-05-21 10:57:47 +0100

    final

--- charles/selection.py
+++ charles/selection.py
@@ -84,7 +84,7 @@ def rank(population):
         raise Exception("No optimization specified (min or max).")
 
     # Sum all ranks
-    total = sum(range(population.size + 1))
+    total = sum(range(population.size_pop + 1))
     # Get a random spin
     spin = uniform(0, total)
     position = 0
